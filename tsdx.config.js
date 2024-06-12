efe744d81275d066d1631ce885282e5089269f28
Ferryal Fajar <ferryal21@gmail.com>
2024-06-12 17:23:29 +0700

    update

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,6 +1,7 @@
 const postcss = require('rollup-plugin-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
